5bbb93124cea9437b4aab7415f096e035b21cc34
micah <micah8719@icloud.com>
2013-09-12 15:28:59 -0400

    Fixed lists concerning orders

--- Magento.RestApi/Json/OrderConverter.cs
+++ Magento.RestApi/Json/OrderConverter.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Magento.RestApi.Models;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Magento.RestApi.Json
 {
@@ -35,8 +36,41 @@ namespace Magento.RestApi.Json
             WriteProperty(product, p => p.total_due, false, writer, serializer);
             WriteProperty(product, p => p.total_paid, false, writer, serializer);
             WriteProperty(product, p => p.total_refunded, false, writer, serializer);
-            WriteProperty(product, p => p.addresses, false, writer, serializer);
-            WriteProperty(product, p => p.order_items, false, writer, serializer);
+            
+            // Lists
+
+            var addresses = product.GetProperty(p => p.addresses);
+            if (addresses != null && addresses.HasChanged())
+            {
+                writer.WritePropertyName("addresses");
+                writer.WriteStartArray();
+                foreach (var address in product.addresses)
+                {
+                    if (address.HasChanged())
+                    {
+                        serializer.Serialize(writer, address);
+                    }
+                }
+                writer.WriteEndArray();
+            }
+
+            var order_items = product.GetProperty(p => p.order_items);
+            if (order_items != null && order_items.HasChanged())
+            {
+                writer.WritePropertyName("order_items");
+                writer.WriteStartArray();
+                foreach (var item in product.order_items)
+                {
+                    if (item.HasChanged())
+                    {
+                        serializer.Serialize(writer, item);
+                    }
+                }
+                writer.WriteEndArray();
+            }
+
+            // WriteProperty(product, p => p.addresses, false, writer, serializer);
+            // WriteProperty(product, p => p.order_items, false, writer, serializer);
 
             writer.WriteEndObject();
         }
@@ -44,8 +78,27 @@ namespace Magento.RestApi.Json
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, Newtonsoft.Json.JsonSerializer serializer)
         {
             var Order = existingValue as Order ?? new Order();
+            //var jObject = JObject.Load(reader);
 
             serializer.Populate(reader, Order);
+            /*Order.Attributes = new Dictionary<string, string>();
+
+            foreach (var item in jObject.Children())
+            {
+                var jProperty = item as JProperty;
+                if (jProperty != null)
+                {
+                    var name = jProperty.Name;
+                    var property = Order.GetType().GetProperty(name);
+                    if (property == null && name != "messages")
+                    {
+                        if (Order.Attributes.ContainsKey(name)) Order.Attributes.Remove(name);
+                        string value = null;
+                        if (jProperty.Value != null) value = jProperty.Value.Value<string>();
+                        Order.Attributes.Add(name, value);
+                    }
+                }
+            }*/
 
             Order.StartTracking();
             return Order;
